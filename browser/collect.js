1a2fad87a9956c0dbabe0a85d6e75b3cb251edfa
yucong02 <yucong02@baidu.com>
2017-10-06 12:51:45 +0800

    update collect.js

--- browser/collect.js
+++ browser/collect.js
@@ -1,7 +1,22 @@
-
-module.exports = function collect(collector) {
+function collect(collector = collect.defaultCollector) {
+	if (typeof collector === 'string') {
+		collector = defaultCollectorMaker(collector)
+	}
     return function (Component) {
         Component['collector'] = collector;
         return Component;
     }
 }
+
+function defaultCollectorMaker(name = 'pageData') {
+	return async function (nextProps) {
+		if (nextProps.pageData) {
+	        let data = await nextProps.pageData()
+	        return {[name]: data};
+	    }
+	    return false;
+	}
+}
+
+collect.defaultCollector = defaultCollectorMaker();
+module.exports = collect;
