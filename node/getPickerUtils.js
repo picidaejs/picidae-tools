7f14a6a2896b64ab7766e4ebaecc907b8a08aa6f
yucong02 <yucong02@baidu.com>
2017-10-09 09:05:42 +0800

    picker::getMarkdownData

--- node/getPickerUtils.js
+++ node/getPickerUtils.js
@@ -1,7 +1,7 @@
 var ID = 0;
 
 function getUtils(metaData, gift, require) {
-    var getHTML = gift.getHTML;
+    var getMarkdownData = gift.getMarkdownData;
     var filename = gift.filename;
     var markdown = gift.content;
 
@@ -9,60 +9,61 @@ function getUtils(metaData, gift, require) {
 
     return {
         getToc: function (html) {
-            var prom = !html ? getHTML() : Promise.resolve(html);
+            var prom = !html ? getMarkdownData() : Promise.resolve(html);
 
             return prom
-                .then(function (html) {
+                .then(function (data) {
+                  html = data.content;
                   // console.log(html);
-                    var id = 'cheerio-' + (ID++);
-                    html = '<' + id + '>' + html + '</' + id + '>'
-                    var dom = cheerio.load(html)(id);
-                    // console.log(html, dom.html())
-                    var toc = [], par = null
-                    dom.children(':header').map(function () {
-                        var self = cheerio(this);
-                        var tagName = self.prop('tagName');
-                        var dep = Number(tagName.replace(/^h/i, ''));
-                        var id = self.attr('id');
-                        var val = self.html();
-                        var node = {dep: dep, id: id, val: val};
+                  var id = 'cheerio-' + (ID++);
+                  html = '<' + id + '>' + html + '</' + id + '>'
+                  var dom = cheerio.load(html)(id);
+                  // console.log(html, dom.html())
+                  var toc = [], par = null
+                  dom.children(':header').map(function () {
+                      var self = cheerio(this);
+                      var tagName = self.prop('tagName');
+                      var dep = Number(tagName.replace(/^h/i, ''));
+                      var id = self.attr('id');
+                      var val = self.html();
+                      var node = {dep: dep, id: id, val: val};
 
-                        if (par) {
-                            if (par.dep < dep) {
-                                par.node.children = par.node.children || [];
-                                par.node.children.push(node);
-                                par = Object.assign({node: node, par: par, dep: node.dep});
-                            }
-                            else {
-                                // if (par.par) {
-                                while (par && par.dep >= dep) {
-                                    par = par.par;
-                                }
-                                if (par) {
-                                    par.node.children = par.node.children || [];
-                                    par.node.children.push(node);
-                                    par = Object.assign({node: node, par: par, dep: node.dep});
-                                }
-                                else {
-                                    toc.push(node);
-                                    par = {node: node, dep: node.dep};
-                                }
+                      if (par) {
+                          if (par.dep < dep) {
+                              par.node.children = par.node.children || [];
+                              par.node.children.push(node);
+                              par = Object.assign({node: node, par: par, dep: node.dep});
+                          }
+                          else {
+                              // if (par.par) {
+                              while (par && par.dep >= dep) {
+                                  par = par.par;
+                              }
+                              if (par) {
+                                  par.node.children = par.node.children || [];
+                                  par.node.children.push(node);
+                                  par = Object.assign({node: node, par: par, dep: node.dep});
+                              }
+                              else {
+                                  toc.push(node);
+                                  par = {node: node, dep: node.dep};
+                              }
 
-                                // }
-                                // else {
-                                //   toc.push(node);
-                                //   par = {node: node, dep: node.dep};
-                                // }
-                                // }
-                            }
-                        }
-                        else {
-                            toc.push(node);
-                            par = {node: node, dep: node.dep};
-                        }
-                    })
+                              // }
+                              // else {
+                              //   toc.push(node);
+                              //   par = {node: node, dep: node.dep};
+                              // }
+                              // }
+                          }
+                      }
+                      else {
+                          toc.push(node);
+                          par = {node: node, dep: node.dep};
+                      }
+                  })
 
-                    return toc;
+                  return toc;
                 })
                 .catch(console.error)
         }
